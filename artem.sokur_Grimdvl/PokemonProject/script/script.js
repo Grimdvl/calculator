e492e1f6b9bc98eba1fc41a6fafbe081a4c8c90a
GrimdvlLaptop <grimdvl@gmail.com>
2023-11-04 13:01:25 +0200

    final push, next exercise cards

--- artem.sokur_Grimdvl/PokemonProject/script/script.js
+++ artem.sokur_Grimdvl/PokemonProject/script/script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // transform value to numbers
     const checkNumInputs = (selector) => {
         const numInputs = document.querySelectorAll(selector);
-
+        
         numInputs.forEach(item => {
             item.addEventListener('input', () => {
                 item.value = item.value.replace(/[^0-9.]/g, '');
@@ -42,17 +42,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Calculator
     const changeState = (state) => {
-        // const firstNumber = document.querySelectorAll('#first-number');
-        // const sign = document.querySelectorAll('#sign');
-        // const secondNumber = document.querySelectorAll('#second-number');
-        const button = document.querySelectorAll('#button');
-
-        // const firstNumberOutput = document.querySelector('.output-first');
-        // const signOutput = document.querySelectorAll('.output-sign');
-        // const secondNumberOutput = document.querySelector('.output-second');
         const numbers = document.querySelectorAll('.number');
         const signs = document.querySelectorAll('.sign');
+        const buttons = document.querySelectorAll('.button');
         const resultOutput = document.querySelector('.output-result');
+        const resultInput = document.querySelector('.input-result');
 
         const calculator = (event, elem, prop) => {
             checkNumInputs('#first-number');
@@ -65,81 +59,53 @@ window.addEventListener('DOMContentLoaded', () => {
                             const inputNumber = numbers[i];
                             if (inputNumber) {
                                 if (i === 0) {
+                                    state[prop] = item.value;
                                     const num1 = +inputNumber.value;
                                     numbers[2].textContent = num1;
-                                    result = calculate(num1, signs[0], numbers[1])
+                                    result = calculate(+num1, signs[0].value, +numbers[1].value)
                                     resultOutput.textContent = result;
-                                    // console.log(num1);
-                                    // console.log(numbers[3]);
                                 } else if (i === 1) {
+                                    state[prop] = item.value;
                                     const num2 = +inputNumber.value;
                                     numbers[3].textContent = num2;
-                                    result = calculate(numbers[3], signs[0], num2)
+                                    result = calculate(+numbers[0].value, signs[0].value, +num2)
                                     resultOutput.textContent = result;
                                 }
                             }
-                            // console.log(item);
-                            // console.log(i);
-                            // if (item.id === 'first-number') {
-                            //     state[prop] = item.value;
-                            //     firstNumberOutput.textContent = item.value;
-                            //     result = calculate(+firstNumberOutput.textContent, sign[0].value, +secondNumberOutput.textContent);
-                            //     resultOutput.textContent = result;
-                            // } else if (item.id === 'second-number') {
-                            //     state[prop] = item.value;
-                            //     secondNumberOutput.textContent = item.value;
-                            //     result = calculate(+firstNumberOutput.textContent, sign[0].value, +secondNumberOutput.textContent);
-                            //     resultOutput.textContent = result;
-                            // }
-                            // state[prop] = i;
-                            // if (item.getAttribute('type') === 'number') {
-                            //     state[prop] = i;
-                                // elem.forEach((num, j) => {
-                                //     item[i].textContent = 
-                                // });
-                            // }
                             break;
                         case 'SELECT':
-                            // const inputSign = signs[i];
-                            if (signs) {
-                                const sign = signs.value;
-                                signs[2].textContent = num1;
-                                result = calculate(num1, signs[0], numbers[1])
-                                resultOutput.textContent = result;
-                                // state[prop] = item.value;
-                                // signOutput.forEach(item =>{
-                                //     item.textContent = item.value;
-                                // });
-                                // result = calculate(+firstNumberOutput.textContent, item.value, +secondNumberOutput.textContent);
-                                // resultOutput.textContent = result;
+                            const inputSign = signs[i];
+                            if (inputSign) {
+                                if (i === 0) {
+                                    state[prop] = item.value;
+                                    const sign = inputSign.value;
+                                    signs[1].textContent = sign;
+                                    result = calculate(+numbers[0].value, sign, +numbers[1].value)
+                                    resultOutput.textContent = result;
+                                }
                             }
                             break;
                         case 'BUTTON':
-                            if (item.id === 'button') {
-                                state[prop] = item.value;
-                                result = calculate(+firstNumberOutput.textContent, sign[0].value, +secondNumberOutput.textContent);
-                                resultOutput.textContent = result;
+                            const buttonOutput = buttons[i];
+                            if (buttonOutput) {
+                                if (i === 0) {
+                                    state[prop] = item.textContent;
+                                    result = calculate(+numbers[0].value, signs[0].value, +numbers[1].value)
+                                    resultInput.textContent = result;
+                                }
                             }
                             break;
                         default:
                             console.log('Error');
                     }
-                    // console.log(state);
+                    console.log(state);
                 });
             });
-
             return result;
         };
-
-        // calculator('input', firstNumber, 'firstNumber');
-        // calculator('input', secondNumber, 'secondNumber');
-        // calculator('change', sign, 'sign');
-        // calculator('change', signOutput, 'sign');
-        calculator('input', numbers, 'firstNumber');
-        // calculator('input', numbers, 'secondNumber');
+        calculator('input', numbers, 'numbers');
         calculator('change', signs, 'sign');
-        calculator('click', button, 'button');
+        calculator('click', buttons, 'button');
     };
-
     changeState(stateInputs);
 });
